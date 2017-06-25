3fa02c0ddda7bf9a005dbe7717251e7668d57a82
Marisa Eisenberg <marisae@users.noreply.github.com>
2017-06-25 09:57:28 -0400

    Create derpltons.R

---
+++ R/derpltons.R
@@ -0,0 +1,13 @@
+# This file is just because I'm curious what linguist does if the repository is exactly 50/50 two languages. 
+
+1+1
+
+dostuff = function(in){
+  in+in
+}
+
+dostuff(2)
+
+dostuff(50)
+
+# be a file and do things etc etc....
