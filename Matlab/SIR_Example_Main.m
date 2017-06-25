21dc1e3013cf6b08a0427b546662ab7ad9741b44
Marisa Eisenberg <marisae@users.noreply.github.com>
2017-06-25 09:40:12 -0400

    merged profile likelihood script into main file

--- Matlab/SIR_Example_Main.m
+++ Matlab/SIR_Example_Main.m
@@ -43,6 +43,7 @@ yfcn = @(x,params) x(:,2)*params(3);
 
 % Starting parameter values:
 params = [0.4, 0.25, 80000]; %beta, gamma, k
+paramnames = {'\beta','\gamma','k'};
 
 
 %% Simulate and Plot the Model
@@ -100,6 +101,46 @@ rank(FIM)
 % CVs = SEs./paramests
 
 
+%% Generate Profile Likelihoods
 
+profiles = [];
+
+% Wrapper function for parameter estimation
+costfun = @(p) sirCost(times,p,data,x0fcn,yfcn);
+    % This is just me being a little lazy---made a wrapper since that's an 
+    % easy way to pass everything you need to fit the parameters (e.g. 
+    % data, initial conditions, measurement eqn, etc.), all in one blob.
+
+threshold = chi2inv(0.95,length(paramests))/2 + fval;
+profrange = 0.25; %percent range for profile to run across
+
+for i=1:length(paramests)
+    %Generate a profile for parameter i, using paramests as the starting
+    %value and the fitter to do the parameter estimation:
+    profiles(:,:,i) = ProfLike(paramests,i,costfun,profrange);
+        % each profile has columns: profiled parameter value, resulting
+        % cost-function (e.g. RSS) value, any flags from the optimizer, and
+        % then columns for each of the other parameter estimates.
+    
+    %plot profile
+    figure(i)
+    set(gca,'LineWidth',1,'FontSize',16,'FontName','Arial')
+    hold on
+    plot(profiles(:,1,i),profiles(:,2,i),'k','LineWidth',2)
+    plot(paramests(i),fval,'r*','LineWidth',2)
+    plot([profiles(1,1,i) profiles(end,1,i)],[threshold threshold],'r--')
+    xlabel(paramnames{i})
+    ylabel('Cost Function Value')
+    
+    %plot parameter relationships
+%     figure(10+i)
+%     set(gca,'LineWidth',1,'FontSize',16,'FontName','Arial')
+%     hold on
+%     plot(profiles(:,1,i),profiles(:,4:end,i),'LineWidth',2)
+%     plot(paramests(i),paramests,'r*')
+%     xlabel(paramlist{i})
+%     ylabel('Estimated Parameter Value')
+%     legend(paramlist)
+end
 
 
