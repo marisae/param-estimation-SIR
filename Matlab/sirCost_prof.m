199c9e844ce1b4de2f3a225f35cf3f7573b5adef
Marisa Eisenberg <marisae@users.noreply.github.com>
2017-06-25 09:34:13 -0400

    Delete sirCost_prof.m

--- Matlab/sirCost_prof.m
+++
@@ -1,9 +0,0 @@
-% Cost Function for the SIR model
-% Marisa Eisenberg 7-29-16 (marisae@umich.edu)
-
-function NLL = sirCost_prof(tspan,params,paramsfixedfcn,data,x0fcn,yfcn)
-params = paramsfixedfcn(abs(params));
-[t,x] = ode45(@sirODE,tspan,x0fcn(params),[],params);
-y = yfcn(x,params);
-% NLL = ((data - y))'*((data - y)); %OLS
-NLL = (y)'*ones(length(y),1) - data'*log(y);  %Poisson ML
