15c35335952d31e09758c5689a7608ad1b4c1435
Marisa Eisenberg <marisae@users.noreply.github.com>
2017-06-29 15:07:23 -0400

    add alternative cost functions

--- Matlab/sirCost.m
+++ Matlab/sirCost.m
@@ -5,6 +5,13 @@ function NLL = sirCost(tspan,params,data,x0fcn,yfcn)
 params = abs(params);
 [t,x] = ode45(@sirODE,tspan,x0fcn(params),[],params);
 y = yfcn(x,params);
+NLL = (y)'*ones(length(y),1) - data'*log(y);  %Poisson ML
+    % missing an additive constant term but it doesn't change the threshold
+    % (and makes calulation easier/faster)
+
+% Least squares - note these are off by a scaling factor so they'll change
+% the threshold
 % NLL = ((data - y))'*((data - y)); %OLS
 % NLL = sum((data-y).^2); % Also OLS! :) 
-NLL = (y)'*ones(length(y),1) - data'*log(y);  %Poisson ML
+% or can use something like this and be in LL units (note requires choosing a sigma!):
+% NLL = -sum(log(normpdf(data,y,0.1*mean(data))));
